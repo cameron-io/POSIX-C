40809de2b0b5ca94e6c18dda716184c0a548cdde
Cameron Gallichan <camerongallichan@gmail.com>
2023-07-04 22:22:24 +0100

    hashtable now multidimensional with bucket depth

--- src/HashTable.c
+++ src/HashTable.c
@@ -8,19 +8,26 @@ typedef struct {
    void* value;
 } Record;
 
-#define SIZE 10
+#define NUM_BUCKETS 10
+#define BUCKET_DEPTH 5
 
-Record* table[SIZE];
+Record* table[NUM_BUCKETS][BUCKET_DEPTH];
 Record* null_terminator;
 
 int ht_hash(char* key) {
-   return strlen(key) % SIZE;
+   return strlen(key) % NUM_BUCKETS;
 }
 
 void ht_init() {
     null_terminator = (Record*) malloc(sizeof(Record));
     null_terminator->value = (void*) NULL;
     null_terminator->key = (void*) NULL;
+
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        for (int j = 0; j < BUCKET_DEPTH; j++) {
+            table[i][j] = null_terminator;
+        }
+    }
 }
 
 void ht_insert(char* key, char* value) {
@@ -29,31 +36,33 @@ void ht_insert(char* key, char* value) {
     record->value = (void*) value;
     record->key = (void*) key;
 
-    while (table[bucket_index] && table[bucket_index]->key) {
-        bucket_index++;
+    int i = 0;
+    while (table[bucket_index][i]->key) {
+        i++;
     }
-    table[bucket_index] = record;
+    table[bucket_index][i] = record;
 }
 
 Record* ht_query(char* key) {
     int bucket_index = ht_hash(key);
- 
-    while(table[bucket_index]) {
-        if(table[bucket_index]->key == key)
-            return table[bucket_index];
-        bucket_index++;
-    }
 
+    int i = 0;
+    while(table[bucket_index][i]) {
+        if(table[bucket_index][i]->key == key)
+            return table[bucket_index][i];
+        i++;
+    }
     return NULL;
 }
 
 Record* ht_delete(char* key) {
     int bucket_index = ht_hash(key);
 
-    while (table[bucket_index]) {
-        if (table[bucket_index]->key == key) {
-            Record* deleted_record = table[bucket_index];
-            table[bucket_index] = null_terminator;
+    int i = 0;
+    while (table[bucket_index][i]) {
+        if (table[bucket_index][i]->key == key) {
+            Record* deleted_record = table[bucket_index][i];
+            table[bucket_index][i] = null_terminator;
             return deleted_record;
         }
         bucket_index++;
@@ -63,11 +72,16 @@ Record* ht_delete(char* key) {
 
 void print_table() {
     printf("[\n");
-    for (int i = 0; i < SIZE; i++) {
-        if (table[i] && table[i]->key)
-            printf("    {\"%s\": \"%s\"}\n", table[i]->key, table[i]->value);
-        else
-            printf("    {\"\": \"\"}\n");
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+        printf("    [");
+        for (int j = 0; j < BUCKET_DEPTH; j++) {
+            if (table[i][j] && table[i][j]->key)
+                printf("{\"%s\": \"%s\"}", table[i][j]->key, table[i][j]->value);
+            else
+                printf("{\"\": \"\"}");
+            if (j < BUCKET_DEPTH - 1) printf(", ");
+        }
+        printf("]\n");
     }
     printf("]\n");
 }
