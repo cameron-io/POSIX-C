5e3cd46459c44f0817527ecf6bf6a5d33c9d90a3
Cameron Gallichan <camerongallichan@gmail.com>
2023-07-07 12:51:20 +0100

    initial server refactoring

--- src/networking/server.c
+++ src/networking/server.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -14,6 +15,43 @@
 #define CLOSESOCKET(s) close(s)
 #define SOCKET int
 #define GETSOCKETERRNO() (errno)
+
+SOCKET socket_init(struct addrinfo *bind_address) {
+    printf("Creating socket...\n");
+    SOCKET new_socket;
+    new_socket = socket(bind_address->ai_family,
+            bind_address->ai_socktype, bind_address->ai_protocol);
+    if (!ISVALIDSOCKET(new_socket)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+    return new_socket;
+}
+
+void socket_set_opt(SOCKET socket) {
+    int option = 0;
+    if (setsockopt(socket, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&option, sizeof(option))) {
+        fprintf(stderr, "setsockopt() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+}
+
+void socket_bind(SOCKET socket, struct addrinfo *bind_address) {
+    printf("Binding socket to local address...\n");
+    if (bind(socket,
+                bind_address->ai_addr, bind_address->ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+}
+
+void socket_listen(SOCKET socket) {
+    printf("Listening...\n");
+    if (listen(socket, 10) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+}
 
 int main() {
     printf("Configuring local address...\n");
@@ -26,48 +64,22 @@ int main() {
     struct addrinfo *bind_address;
     getaddrinfo(0, "8080", &hints, &bind_address);
 
-
-    printf("Creating socket...\n");
-    SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-            bind_address->ai_socktype, bind_address->ai_protocol);
-    if (!ISVALIDSOCKET(socket_listen)) {
-        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
-
-    int option = 0;
-    if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&option, sizeof(option))) {
-        fprintf(stderr, "setsockopt() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
-
-    printf("Binding socket to local address...\n");
-    if (bind(socket_listen,
-                bind_address->ai_addr, bind_address->ai_addrlen)) {
-        fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
+    SOCKET socket = socket_init(bind_address);
+    socket_set_opt(socket);
+    socket_bind(socket, bind_address);
     freeaddrinfo(bind_address);
 
-
-    printf("Listening...\n");
-    if (listen(socket_listen, 10) < 0) {
-        fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
-        return 1;
-    }
-
+    socket_listen(socket);
 
     printf("Waiting for connection...\n");
     struct sockaddr_storage client_address;
     socklen_t client_len = sizeof(client_address);
-    SOCKET socket_client = accept(socket_listen,
+    SOCKET socket_client = accept(socket,
             (struct sockaddr*) &client_address, &client_len);
     if (!ISVALIDSOCKET(socket_client)) {
         fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
         return 1;
     }
-
 
     printf("Client is connected... ");
     char address_buffer[100];
@@ -76,13 +88,11 @@ int main() {
             NI_NUMERICHOST);
     printf("%s\n", address_buffer);
 
-
     printf("Reading request...\n");
     char request[1024];
     int bytes_received = recv(socket_client, request, 1024, 0);
     printf("Received %d bytes.\n", bytes_received);
     printf("%.*s", bytes_received, request);
-
 
     printf("Sending response...\n");
     const char *response =
@@ -99,12 +109,11 @@ int main() {
     bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
     printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
 
-
     printf("Closing connection...\n");
     CLOSESOCKET(socket_client);
 
     printf("Closing listening socket...\n");
-    CLOSESOCKET(socket_listen);
+    CLOSESOCKET(socket);
 
     printf("Finished.\n");
 
