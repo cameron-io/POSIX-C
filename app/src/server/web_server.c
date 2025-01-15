7231c59d982444cecc98e2cc1c6f40f15d1bef5f
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-15 18:26:22 +0000

    refactored web-server application

--- app/src/server/web_server.c
+++ app/src/server/web_server.c
@@ -6,14 +6,61 @@
 #include "web_server/manager.h"
 #include "web_server/responses.h"
 
+void handle_requests(struct client_info *client_list, fd_set* reads) {
+    struct client_info *client = client_list;
+
+    while(client) {
+        struct client_info *next = client->next;
+
+        if (FD_ISSET(client->socket, reads)) {
+
+            if (MAX_REQUEST_SIZE == client->received) {
+                send_400(&client_list, client);
+                client = next;
+                continue;
+            }
+
+            int r = recv(client->socket,
+                    client->request + client->received,
+                    MAX_REQUEST_SIZE - client->received, 0);
+
+            if (r < 1) {
+                printf("Unexpected disconnect from %s.\n",
+                        get_client_address(client));
+                drop_client(&client_list, client);
+                client = next;
+                continue;
+            }
+            client->received += r;
+            client->request[client->received] = 0;
+
+            char *q = strstr(client->request, "\r\n\r\n");
+            if (q) {
+                *q = 0;
+
+                if (strncmp("GET /", client->request, 5)) {
+                    send_400(&client_list, client);
+                } else {
+                    char *path = client->request + 4;
+                    char *end_path = strstr(path, " ");
+                    if (!end_path) {
+                        send_400(&client_list, client);
+                    } else {
+                        *end_path = 0;
+                        serve_resource(&client_list, client, path);
+                    }
+                }
+            } //if (q)
+        }
+        client = next;
+    }
+}
+
 int main() {
-
     SOCKET server = create_socket(0, "8080");
-
     struct client_info *client_list = 0;
 
     while(1) {
-
         fd_set reads;
         reads = wait_on_clients(&client_list, server);
 
@@ -32,56 +79,8 @@ int main() {
 
             printf("New connection from %s.\n",
                     get_client_address(client));
+            handle_requests(client_list, &reads);
         }
-
-        struct client_info *client = client_list;
-        while(client) {
-            struct client_info *next = client->next;
-
-            if (FD_ISSET(client->socket, &reads)) {
-
-                if (MAX_REQUEST_SIZE == client->received) {
-                    send_400(&client_list, client);
-                    client = next;
-                    continue;
-                }
-
-                int r = recv(client->socket,
-                        client->request + client->received,
-                        MAX_REQUEST_SIZE - client->received, 0);
-
-                if (r < 1) {
-                    printf("Unexpected disconnect from %s.\n",
-                            get_client_address(client));
-                    drop_client(&client_list, client);
-
-                } else {
-                    client->received += r;
-                    client->request[client->received] = 0;
-
-                    char *q = strstr(client->request, "\r\n\r\n");
-                    if (q) {
-                        *q = 0;
-
-                        if (strncmp("GET /", client->request, 5)) {
-                            send_400(&client_list, client);
-                        } else {
-                            char *path = client->request + 4;
-                            char *end_path = strstr(path, " ");
-                            if (!end_path) {
-                                send_400(&client_list, client);
-                            } else {
-                                *end_path = 0;
-                                serve_resource(&client_list, client, path);
-                            }
-                        }
-                    } //if (q)
-                }
-            }
-
-            client = next;
-        }
-
     } //while(1)
 
     printf("\nClosing socket...\n");
